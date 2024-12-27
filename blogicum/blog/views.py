849bde8061123ee68298adff0f36aeebecbf02d4
Michail Vavilov <MihailVavilov186@yandex.ru>
2024-12-27 16:05:10 +0700

    third-commit

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -5,40 +5,34 @@ from django.utils import timezone
 
 from .forms import CommentForm, PostForm, ProfileEditForm
 from .models import Category, Comment, Post, User
-from .service import paginate_content, fetch_posts
+from .service import get_paginator, get_posts
 
 
 def index(request):
-    posts = fetch_posts(Post.objects.select_related(
-        "author", "category")).order_by("-pub_date")
-    page = paginate_content(request, posts)
-    return render(request, "blog/index.html", {"page_obj": page})
+    post_list = get_posts(Post.objects.select_related("author", "category")).order_by(
+        "-pub_date"
+    )
+    page_obj = get_paginator(request, post_list)
+    return render(request, "blog/index.html", {"page_obj": page_obj})
 
 
 def post_detail(request, post_id):
-    post = get_object_or_404(Post, id=post_id)
-    if request.user != post.author:
-        post = get_object_or_404(fetch_posts(Post.objects), id=post_id)
-    comments = post.comments.order_by("created_at")
-    comment_form = CommentForm()
+    posts = get_object_or_404(Post, id=post_id)
+    if request.user != posts.author:
+        posts = get_object_or_404(get_posts(Post.objects), id=post_id)
+    comments = posts.comments.order_by("created_at")
+    form = CommentForm()
     return render(
-        request,
-        "blog/detsil.html",
-        {"post": post, "form": comment_form, "comments": comments},
+        request, "blog/detail.html", {"post": posts, "form": form, "comments": comments}
     )
 
 
-def category_posts(request, slug):
-    selected_category = get_object_or_404(Category, slug=slug, is_published=True)
-    posts = fetch_posts(selected_category.posts.select_related("author")).order_by(
-        "-pub_date"
-    )
-    page = paginate_content(request, posts)
-    return render(
-        request,
-        "blog/category.html",
-        {"category": selected_category, "page_obj": page},
-    )
+def category_posts(request, category_slug):
+    category = get_object_or_404(Category, slug=category_slug, is_published=True)
+    post_list = get_posts(category.posts.select_related("author")).order_by("-pub_date")
+    page_obj = get_paginator(request, post_list)
+    context = {"category": category, "page_obj": page_obj}
+    return render(request, "blog/category.html", context)
 
 
 @login_required
@@ -49,29 +43,29 @@ def create_post(request):
         post.author = request.user
         post.save()
         return redirect("blog:profile", request.user)
-    return render(request, "blog/create.html", {"form": form})
+    context = {"form": form}
+    return render(request, "blog/create.html", context)
 
 
 def profile(request, username):
-    user_instance = get_object_or_404(User, username=username)
-    current_time = timezone.now()
-    if user_instance.is_authenticated:
-        posts = (
-            user_instance.posts.annotate(comment_count=Count("comments"))
+    user = get_object_or_404(User, username=username)
+    now = timezone.now()
+    if user.is_authenticated:
+        posts_list = (
+            user.posts.annotate(comment_count=Count("comments"))
             .select_related("author")
             .order_by("-pub_date")
         )
     else:
-        posts = (
-            user_instance.posts.filter(is_published=True, pub_date__lte=current_time)
+        posts_list = (
+            user.posts.filter(is_published=True, pub_date__lte=now)
             .annotate(comment_count=Count("comments"))
             .select_related("author")
             .order_by("-pub_date")
         )
-    page_obj = paginate_content(request, posts)
-    return render(
-        request, "blog/profile.html", {"profile": user_instance, "page_obj": page_obj}
-    )
+    page_obj = get_paginator(request, posts_list)
+    context = {"profile": user, "page_obj": page_obj}
+    return render(request, "blog/profile.html", context)
 
 
 @login_required
@@ -85,64 +79,65 @@ def edit_profile(request):
 
 @login_required
 def edit_post(request, post_id):
-    post_instance = get_object_or_404(Post, id=post_id)
-    if request.user != post_instance.author:
-        return redirect("blog:post_detail", post_id=post_id)
-    form = PostForm(
-        request.POST or None, files=request.FILES or None, instance=post_instance
-    )
+    post = get_object_or_404(Post, id=post_id)
+    if request.user != post.author:
+        return redirect("blog:post_detail", post_id)
+    form = PostForm(request.POST, files=request.FILES or None, instance=post)
     if form.is_valid():
-        form.save()
-        return redirect("blog:post_detail", post_id=post_id)
-    return render(request, "blog/post_form.html", {"form": form})
+        post.save()
+        return redirect("blog:post_detail", post_id)
+    form = PostForm(instance=post)
+    context = {"form": form}
+    return render(request, "blog/create.html", context)
 
 
 @login_required
 def delete_post(request, post_id):
-    post_instance = get_object_or_404(Post, id=post_id)
-    if request.user != post_instance.author:
-        return redirect("blog:post_detail", post_id=post_id)
+    post = get_object_or_404(Post, id=post_id)
+    if request.user != post.author:
+        return redirect("blog:post_detail", post_id)
     if request.method == "POST":
-        form = PostForm(request.POST or None, instance=post_instance)
-        post_instance.delete()
+        form = PostForm(request.POST or None, instance=post)
+        post.delete()
         return redirect("blog:index")
-    form = PostForm(instance=post_instance)
-    return render(request, "blog/create.html", {"form": form})
+    form = PostForm(instance=post)
+    context = {"form": form}
+    return render(request, "blog/create.html", context)
 
 
 @login_required
 def add_comment(request, post_id):
-    post_instance = get_object_or_404(Post, id=post_id)
+    post = get_object_or_404(Post, id=post_id)
     form = CommentForm(request.POST or None)
     if form.is_valid():
         comment = form.save(commit=False)
-        comment.post = post_instance
+        comment.post = post
         comment.author = request.user
         comment.save()
-    return redirect("blog:post_detail", post_id=post_id)
+    return redirect("blog:post_detail", post_id)
 
 
 @login_required
 def edit_comment(request, post_id, comment_id):
-    comment_instance = get_object_or_404(Comment, id=comment_id)
-    if request.user != comment_instance.author:
-        return redirect("blog:post_detail", post_id=post_id)
-    form = CommentForm(request.POST or None, instance=comment_instance)
+    comment = get_object_or_404(Comment, id=comment_id)
+    if request.user != comment.author:
+        return redirect("blog:post_detail", post_id)
+    form = CommentForm(request.POST or None, instance=comment)
     if form.is_valid():
         form.save()
-        return redirect("blog:post_detail", post_id=post_id)
-    form = CommentForm(instance=comment_instance)
-    return render(
-        request, "blog/comment.html", {"form": form, "comment": comment_instance}
-    )
+        return redirect("blog:post_detail", post_id)
+    form = CommentForm(instance=comment)
+    context = {"form": form, "comment": comment}
+    return render(request, "blog/comment.html", context)
 
 
 @login_required
 def delete_comment(request, post_id, comment_id):
-    comment_instance = get_object_or_404(Comment, id=comment_id)
-    if request.user != comment_instance.author:
-        return redirect("blog:post_detail", post_id=post_id)
+    comment = get_object_or_404(Comment, id=comment_id)
+    if request.user != comment.author:
+        return redirect("blog:post_detail", post_id)
     if request.method == "POST":
-        comment_instance.delete()
-        return redirect("blog:post_detailw", post_id=post_id)
-    return render(request, "blog/comment.html", {"comment": comment_instance})
+        comment.delete()
+        return redirect("blog:post_detail", post_id)
+    context = {"comment": comment}
+    return render(request, "blog/comment.html", context)
